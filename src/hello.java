e7c7ce5292eb8113c07cef481e18bd67d94d1499
dongjiawei <dongjiawei_qd@caxins.com>
2018-08-01 18:17:36 +0800

    哈哈哈哈

---
+++ src/hello.java
@@ -0,0 +1,12 @@
+/**
+ * @author : dongjiawei
+ * @Description : 哈哈哈
+ * @date : 2018/08/01 18:13
+ * @system name:新一代消费金融系统
+ * @copyright:长安新生（深圳）金融投资有限公司
+ */
+public class hello {
+    public String show(){
+        return null;
+    }
+}
