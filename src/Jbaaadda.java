f3cc5a82ec7e8b0e6e1e7c592aaab9801339d8b8
dongjiawei <dongjiawei_qd@caxins.com>
2018-08-01 18:22:47 +0800

    哈哈哈哈

---
+++ src/Jbaaadda.java
@@ -0,0 +1,11 @@
+/**
+ * @author : dongjiawei
+ * @Description : 哈哈哈
+ * @date : 2018/08/01 18:22
+ * @system name:新一代消费金融系统
+ * @copyright:长安新生（深圳）金融投资有限公司
+ */
+public class Jbaaadda {
+
+    public
+}
